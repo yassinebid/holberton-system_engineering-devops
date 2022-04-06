d38d00986d854729713717912b3a982b20a17ad2
yassinebid <3237@holbertonschool.com>
2022-04-06 18:30:25 +0100

    fix

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+# Using Puppet, create a file in /tmp
+file { '/tmp/school':
+    content => 'I love Puppet',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+  }
--- 0x0A-configuration_management/README.md
+++ 0x0A-configuration_management/README.md
@@ -1,2 +1,1 @@
-0x0A. Configuration management
-
+# 0x0A-configuration_management
